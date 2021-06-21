187a9e26b3bc2699970f408b47edb4fdc1a2c744
Serhii Unhurian <unguryan23@gmail.com>
2021-06-21 21:36:12 +0300

    feat: post movies to firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import MoviesList from './components/MoviesList';
 import AddMovie from './components/AddMovie';
+import MoviesList from './components/MoviesList';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -13,21 +13,25 @@ function App() {
     setError(null);
 
     try {
-      const response = await fetch('https://swapi.dev/api/films');
+      const response = await fetch(
+        'https://next-starter-bc9e6-default-rtdb.europe-west1.firebasedatabase.app/movies.json'
+      );
 
       if (!response.ok) {
         throw new Error(`Error: ${response.statusText}`);
       }
 
       const data = await response.json();
-      const transformedMovies = data.results.map(movieData => ({
-        id: movieData.episode_id,
-        title: movieData.title,
-        releaseDate: movieData.release_date,
-        openingText: movieData.opening_crawl,
-      }));
+      const loadedMovies = [];
 
-      setMovies(transformedMovies);
+      for (const [id, movie] of Object.entries(data)) {
+        loadedMovies.push({
+          id,
+          ...movie,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -40,8 +44,32 @@ function App() {
   }, []);
 
   const handleFetchMovies = () => loadMovies();
-  const handleAddMovie = movie => {
-    console.log(movie);
+  const handleAddMovie = async movie => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(
+        'https://next-starter-bc9e6-default-rtdb.europe-west1.firebasedatabase.app/movies.json',
+        {
+          method: 'POST',
+          body: JSON.stringify(movie),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+
+      setMovies(prevMovies => [movie, ...prevMovies]);
+    } catch (error) {
+      setError(error.message);
+    }
+
+    setIsLoading(false);
   };
 
   let content = <p>No movies.</p>;
